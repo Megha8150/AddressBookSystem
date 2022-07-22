1364e0402b57d3044b879032bcbcfb290d6aaa0b
Megha8150 <107126853+Megha8150@users.noreply.github.com>
2022-07-23 01:52:52 +0530

    Update AddressBook.cs

--- AddressBookSystem/AddressBook.cs
+++ AddressBookSystem/AddressBook.cs
@@ -9,33 +9,7 @@ namespace AddressBookSystem
     public class AddressBook
     {
         public List<Contact> addressBook = new List<Contact>();
-        //public void AddNewContact()
-        //{
-        //    Contact contact = new Contact();
-
-        //    Console.WriteLine("enter first name");
-        //    contact.FirstName = Console.ReadLine();
-
-        //    Console.WriteLine("enter second name");
-        //    contact.LastName = Console.ReadLine();
-
-        //    Console.WriteLine("enter address name");
-        //    contact.Address = Console.ReadLine();
-
-        //    Console.WriteLine("enter phno name");
-        //    contact.PhNo = Console.ReadLine();
-
-        //    Console.WriteLine("enter city name");
-        //    contact.City = Console.ReadLine();
-
-        //    Console.WriteLine("enter state name");
-        //    contact.State = Console.ReadLine();
-
-        //    Console.WriteLine("enter zip name");
-        //    contact.Zip = Console.ReadLine();
-
-        //    addressBook.Add(contact);
-        //}
+    
         public void AddPerson()
         {
             Contact person = new Contact();
